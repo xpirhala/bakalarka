34d3e75c2fc0971af744045a4ae3f6b449fa824c
Matej <matejpirhala@gmail.com>
2019-11-10 00:02:56 +0100

    Pridanie main funkcie

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import cv2
+import obrazy
+import test
+
+from PIL import Image
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+
+
