6caa7c6a700e5ddf952ecb7e54c917fe551f5114
Matej <matejpirhala@gmail.com>
2019-11-10 11:28:51 +0100

    Oprava elementov + prva vetva

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ This is a temporary script file.
 import cv2
 import obrazy
 import test
-
+import numpy
 from PIL import Image
 import cv2
 import numpy as np
@@ -41,18 +41,97 @@ inputImage = cv2.imread('test2.TIF')
 
 
 #creating mask/fundus
-img=createFundusMask(inputImage)
+fundus_mask=createFundusMask(inputImage)
 
 
 #elementarne prvky
 se=morphology.octagon(9,8)
-se1=morphology.disk(5)
-se2=morphology.disk(3)
-se11=morphology.disk(6)
-se22=morphology.disk(3)
 
-i=inputImage[:,:,2]
+se1=morphology.disk(5)
+se1=numpy.delete(se1, 0, 0)
+se1=numpy.delete(se1, 9, 0)
+se1=numpy.delete(se1, 0, 1)
+se1=numpy.delete(se1, 9, 1)
+
+se2=morphology.disk(3)
+se2=numpy.delete(se2, 0, 0)
+se2=numpy.delete(se2, 5, 0)
+se2=numpy.delete(se2, 0, 1)
+se2=numpy.delete(se2, 5, 1)
+
+se11=morphology.disk(6)
+se11=numpy.delete(se11, 0, 0)
+se11=numpy.delete(se11, 11, 0)
+se11=numpy.delete(se11, 0, 1)
+se11=numpy.delete(se11, 11, 1)
+
+se22=morphology.disk(3)
+se22=numpy.delete(se22, 0, 0)
+se22=numpy.delete(se22, 5, 0)
+se22=numpy.delete(se22, 0, 1)
+se22=numpy.delete(se22, 5, 1)
+
 #Prva vetva
 
 #Morfologicke otvorenie
-Img1=morphology.binary_opening(i,se)
+i=inputImage[:,:,2] #vyberieme greenpicture
+
+Img1=morphology.opening(i,se2)
+plt.imshow(Img1,cmap=plt.cm.gray)
+plt.show()
+
+Img1=morphology.reconstruction(Img1, i)
+plt.imshow(Img1,cmap=plt.cm.gray)
+plt.show()
+
+
+Img1=Img1-morphology.white_tophat(Img1,se1 )-morphology.black_tophat(Img1, se1) #subtract
+plt.imshow(Img1,cmap=plt.cm.gray)
+plt.show()
+
+Img2=morphology.closing(Img1, se)
+plt.imshow(Img2,cmap=plt.cm.gray)
+plt.show()
+
+Img2=Img2-Img1
+plt.imshow(Img2,cmap=plt.cm.gray)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
